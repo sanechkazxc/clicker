95d4d277ac2257fc1af0ae9d30091712c7bf112b
sanechkazxc <125720703+sanechkazxc@users.noreply.github.com>
2023-03-12 09:28:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+function plus() {
+ document.getElementById('clicker').innerText = count;
+ count++;
+
+}
+function minus() {
+    document.getElementById('clicker').innerText = count;
+    count--;
+   
+   }
